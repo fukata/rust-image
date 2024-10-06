99c5efb0854c4856f0a6098212c7d60db60c4cf4
fukata <tatsuya.fukata@gmail.com>
2024-10-06 23:00:07 +0900

    ref load image

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use wasm_bindgen::prelude::*;
-use image::{load_from_memory, ImageOutputFormat, GenericImageView};
-
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use image::{load_from_memory, ImageOutputFormat};
 
 #[wasm_bindgen]
 pub fn resize_image(image_data: &[u8], new_width: u32, new_height: u32, output_format: Option<String>) -> Vec<u8> {
@@ -31,18 +28,4 @@ pub fn resize_image(image_data: &[u8], new_width: u32, new_height: u32, output_f
 
     resized.write_to(&mut buffer, format).expect("Failed to write image");
     buffer
-}
-
-#[wasm_bindgen]
-#[derive(Clone)]
-pub struct ImageInfo {
-    pub width: u32,
-    pub height: u32,
-}
-
-#[wasm_bindgen]
-pub fn get_image_info(image_data: &[u8]) -> ImageInfo {
-    let img = load_from_memory(image_data).expect("Failed to load image");
-    let (width, height) = img.dimensions();
-    ImageInfo { width, height }
 }
